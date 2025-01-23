fd65695d8c0c00a2736aa658768f49067ee4e92e
DESKTOP-65SAUST\Alumno <adrian.cano@udit.es>
2025-01-23 15:22:52 +0100

    coin flipping

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
 
-    console.log("Online jQuery!");
-    
+    let $coin = $("#coin");
+
+    $coin.on("click", function(){
+        let randomNum = Math.random();
+        $coin.removeClass();
+
+        setTimeout(function(){
+            if (randomNum <= 0.5) {
+                console.log("Es cara!");
+                $coin.addClass("isHeads");
+            } else {
+                console.log("Es cruz!");
+                $coin.addClass("isTails");
+            }
+        }, 100);
+         
+    });
 
 });
